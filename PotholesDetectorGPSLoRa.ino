a22b3dca95cda60584870046d708e765c309f3b2
Claudio Pastorini <pastorini.claudio@gmail.com>
2018-05-18 15:40:43 +0200

    MVP demo code

--- PotholesDetectorGPSLoRa.ino
+++ PotholesDetectorGPSLoRa.ino
@@ -85,6 +85,13 @@ uint8_t sentCounter = 0;
 uint8_t receivedCounter = 0;
 
 //TODO creates fake data to feed the device for demo
+const char *gpsStream =
+  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
+  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
+  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
+  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
+  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
+  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n"; 
 
 void setup() {
 #if defined(ARDUINO_ARCH_SEEEDUINO_SAMD)
@@ -162,8 +169,8 @@ void loop() {
     position = 0;
     IS_MESSAGE_COMPLETE = false;
   //}
-
-  delay(100);
+  
+  delay(1500);
 #elif defined(ARDUINO_SAMD_FEATHER_M0)
   char *message = NULL;
   short rssi = 0;
@@ -215,6 +222,23 @@ void getGPSData(void (*callback)(TinyGPSPlus *)) {
         }
       } else {
         SERIAL.println("No data");
+
+        while (*gpsStream) {
+          if (gps.encode(*gpsStream++)) {
+            if (gps.location.isValid() && gps.location.isUpdated()) {
+              // If there is a callback
+              if (callback != NULL) {
+                // Call the callback
+                (*callback)(&gps);
+              // Otherwise  
+              } else {
+                // Print the GPS info
+                printGPSInfo(&gps);
+              } 
+            }
+          }
+        }
+       
       }
     }
   }
@@ -280,7 +304,8 @@ void generateAndSendPotholeInfo(TinyGPSPlus *gps_p) {
   // Adds the date from the GPS
   message += ",";
   if (gps.date.isValid()) {
-    message += gps.date.value();
+    //message += gps.date.value();
+    message += "180518";
   } else {
     message += "0";
   }
@@ -366,7 +391,7 @@ bool receiveMessage(char **message, short *rssi, uint8_t seconds) {
 #elif defined(ARDUINO_SAMD_FEATHER_M0)
   // Waits for a message
   if (!rf95.waitAvailableTimeout(1 * MILLIS)) { 
-    SERIAL.println("No message, is there a transmitter around?");
+    //SERIAL.println("No message, is there a transmitter around?");
     return false;
   }
   
